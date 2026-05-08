a6d77ae061975103d2fc9bb93176e5199aafa4ec
João Semedo <25249@ipportalegre.pt>
2026-05-08 10:43:23 +0100

    Correção na route post dos users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const db = require('../db_connection/db');
 const app = express();
 
+app.use(express.json());
+
 app.get('/users/:id',(req,res)=> {
   db.query('select * from users where id=?', [req.params.id], (err, result) => {
     if (err) {
@@ -16,16 +18,17 @@ app.get('/users/:id',(req,res)=> {
 
 
 app.post('/users',(req,res)=>{
-  const {fullName,gender,birthDate,maritalStatus,city,country,email,phoneNumber}=req.body;
-  db.query('Insert into users(fullName,gender,birthDate,maritalStatus,city,country,email,phoneNumber) values(?,?,?,?,?,?,?,?)',[fullName,gender,birthDate,maritalStatus,city,country,email,phoneNumber],(err,result)=>{
+  const {fullName,loginPassword,gender,birthDate,maritalStatus,city,country,email,phoneNumber}=req.body;
+  db.query('Insert into users(fullName,loginPassword,gender,birthDate,maritalStatus,city,country,email,phoneNumber) values(?,?,?,?,?,?,?,?,?)',[fullName,loginPassword,gender,birthDate,maritalStatus,city,country,email,phoneNumber],(err,result)=>{
     if (err) {
       return res.status(500).json(err);
     }
-    res.json({id:result.insertId,fullName,gender,birthDate,maritalStatus,city,country,email,phoneNumber});
+    res.json({id:result.insertId,fullName,loginPassword,gender,birthDate,maritalStatus,city,country,email,phoneNumber});
   })
 })
 
 app.listen(3001, () => {
   console.log('Servidor Express a correr na porta 3001!');
 });
+
 module.exports = router;
