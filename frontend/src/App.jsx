2a32fd3e2aa707c50191986472d2bc3d210509d6
leonorcaldeira9 <leonorcaldeira2018@gmail.com>
2026-05-12 13:19:12 +0100

    adicionado app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -120,3 +120,21 @@ function App() {
 }
 
 export default App
+*/
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Register from "./pages/register/Register.jsx";
+import Login from "./pages/login/Login.jsx";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App;
